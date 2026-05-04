4a1038e6e6d6549187f17c0746ba360418ae931d
Vitor Mafra <vitor.mafra@dcc.ufmg.br>
2026-05-03 22:27:22 -0300

    chore: configure custom domain for kinderovo.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://vitor-mafra.github.io",
-  base: "/kinder-ovo",
+  site: "https://kinderovo.com",
 });
